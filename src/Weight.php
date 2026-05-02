19ca1c0510db670814b92f8235312e619a516210
unknown <abdulqdosalabinie@gmail.com>
2026-05-02 15:04:54 +0200

    test wight

---
+++ src/Weight.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AbdulqdosAlabinie\WeightConversions;
+
+class Weight
+{
+    public static function kilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+
+    public function __construct(float $kilograms)
+    {
+        $this->kilograms = $kilograms;
+    }
+
+    public function toPounds(): float
+    {
+        return $this->kilograms * 2.2046 ;
+    }
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,13 @@
 <?php
 
+
+use AbdulqdosAlabinie\WeightConversions\Weight;
+
 it('can test', function () {
     expect(true)->toBeTrue();
+});
+
+it('can convert kilograms to pounds', function () {
+    $pounds = Weight::kilograms(100)->toPounds();
+    expect($pounds)->toEqual(220.46);
 });
